9ae9ecc876e4d9bd9d53dc009fe5261e355a7b91
Huy Tran <huytrn13@gmail.com>
2025-05-10 01:21:38 -0500

    Added filter functionality to project button

--- src/index.js
+++ src/index.js
@@ -28,6 +28,58 @@ addProjectButton.addEventListener('click', () => {
 })
 
 
+// project container
+const projectContainer = document.querySelector('.project-container');
+
+// form project list
+const projectList = document.querySelector('#project');
+
+
+// adding project
+const addProject = document.querySelector('.add-project');
+addProject.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // Getting project name
+    const projectName = document.querySelector('#projectName').value;
+
+    // check for dup project name 
+    const dupName = storage.projects.find(p => p.name === projectName);
+
+    // if no dup
+    if(!dupName){
+        const newProject = createProject(projectName);
+        storage.projects.push(newProject);
+        // projectForm.classList.add('hide');
+
+        // add project button
+        const projectButton = document.createElement('button');
+        projectButton.classList.add('project');
+        projectButton.textContent = projectName;
+
+        // project button filter to its own tasks
+
+        projectButton.addEventListener('click', () => displayProjectTasks(projectName));
+
+        projectContainer.appendChild(projectButton);
+
+
+        // add to project list in task form
+        const projectOption = document.createElement('option');
+        projectOption.textContent = projectName;
+        projectOption.value = projectName;
+
+        projectList.appendChild(projectOption);
+        
+    }
+
+    projectForm.reset();
+    projectForm.classList.add('hide');
+
+
+})
+
+
 // New Task Button
 const submitTask = document.querySelector('.submit-task');
 submitTask.addEventListener('click', (e) => {
@@ -43,6 +95,7 @@ submitTask.addEventListener('click', (e) => {
     const priority = document.querySelector('input[name="priority"]:checked').value;
 
     const project = document.querySelector('#project').value;
+
     const newTask = createTask(taskTitle, taskDesc, taskDate, priority, project);
 
     // console.log(project)
@@ -50,11 +103,21 @@ submitTask.addEventListener('click', (e) => {
 
     storage.tasks.push(newTask);
 
+    // add task to associate project if there is project value 
+    if(project){
+        const projectName = storage.projects.find(p => p.name === project);
+        projectName.addTask(newTask);
+        displayProjectTasks(project);
+    } else{
+        displayAllTasks();
+    }
+
     document.querySelector('.taskForm').reset();
 
     taskForm.classList.add('hide');
 
-    displayAllTasks();
+
+    
     
 });
 
@@ -65,9 +128,17 @@ function taskCard(title, dueDate){
     taskTitle.textContent = title;
     const taskDate = document.createElement('h3');
     taskDate.textContent = dueDate;
+    // complete button toggle 
+    const completeButton = document.createElement('button');
+    completeButton.addEventListener('click', (e) => {
+        const curTask = e.target;
+        curTask.toggleComplete();
+    })
+
 
     taskDiv.appendChild(taskTitle);
     taskDiv.appendChild(taskDate);
+    taskDiv.appendChild(completeButton);
 
     return taskDiv;
 }
@@ -88,6 +159,23 @@ function displayAllTasks() {
 
 function displayProjectTasks(project){
     const tasksContainer = document.querySelector('.tasks-container');
+
+    tasksContainer.innerHTML = '';
+
+
+    const targetProject = storage.projects.find(p => p.name === project);
+    targetProject.tasks.forEach((task) => {
+        const taskTitle = task.title;
+        const taskDueDate = task.dueDate;
+
+        tasksContainer.appendChild(taskCard(taskTitle, taskDueDate));
+    })
 }
 
-// window.storage = storage
+
+const allTasksButton = document.querySelector('.allTasks');
+allTasksButton.addEventListener('click', displayAllTasks);
+
+
+
+window.storage = storage
